e296f29a549e5278734e87608f8b8938008b0083
WaiLi <WaiLi_77@outlook.com>
2024-12-01 13:44:31 +0800

    init

---
+++ src/main/java/me/rui/fdyzrank/FdyzRankApplication.java
@@ -0,0 +1,13 @@
+package me.rui.fdyzrank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdyzRankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FdyzRankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/rui/fdyzrank/FdyzRankApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rui.fdyzrank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdyzRankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
