14210d69efea63214a74cab7a27957938967198a
WaiLi <WaiLi_77@outlook.com>
2024-12-15 15:42:23 +0800

    :rocket: 完成依赖配置

---
+++ src/main/java/me/rui/fdyzrank/FdyzRankConfig.java
@@ -0,0 +1,12 @@
+package me.rui.fdyzrank;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "fdyz-rank")
+public class FdyzRankConfig {
+    private String answer;
+}
