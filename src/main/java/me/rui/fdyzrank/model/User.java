0d04925895f67d472bf0e982a75ef77b1ae6c5cf
WaiLi <WaiLi_77@outlook.com>
2024-12-22 16:12:15 +0800

    :art: 优化import

--- src/main/java/me/rui/fdyzrank/model/User.java
+++ src/main/java/me/rui/fdyzrank/model/User.java
@@ -1,9 +1,7 @@
 package me.rui.fdyzrank.model;
 
 import com.mybatisflex.annotation.Id;
-import com.mybatisflex.annotation.KeyType;
 import com.mybatisflex.annotation.Table;
-import com.mybatisflex.core.keygen.KeyGenerators;
 import lombok.Data;
 
 @Data
