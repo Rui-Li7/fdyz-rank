17ff8f34ae672a0c90c374efaccbb82afc3ed53c
WaiLi <WaiLi_77@outlook.com>
2025-05-18 09:37:20 +0800

    :beers: 提交一大堆*3

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,4 +1,3 @@
-use fdyz_rank;
 create table class
 (
     id      bigint unsigned              not null
@@ -19,19 +18,27 @@ create table score
 (
     user_id    bigint unsigned not null,
     teacher_id bigint unsigned not null,
-    score      tinyint         not null
+    score      tinyint         not null,
+    verified   tinyint(1)      not null
 );
+
+create index score_user_id_teacher_id_index
+    on score (user_id, teacher_id);
+
+create index score_verified_index
+    on score (verified);
 
 create table teacher
 (
-    id        bigint unsigned not null
+    id         bigint unsigned not null
         primary key,
-    global_id bigint unsigned not null,
-    is_male   tinyint(1)      not null,
-    name      varchar(10)     not null,
-    subject   varchar(10)     not null,
-    class_id  bigint unsigned null,
-    score     tinyint         not null,
+    global_id  bigint unsigned not null,
+    is_male    tinyint(1)      not null,
+    name       varchar(10)     not null,
+    subject    varchar(10)     not null,
+    class_id   bigint unsigned not null,
+    score      decimal         not null,
+    vote_count int             not null,
     constraint teacher_pk_2
         unique (id)
 );
